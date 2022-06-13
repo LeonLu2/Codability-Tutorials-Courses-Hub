9e78a9d2edda8f79ea877847624bb59624adf447
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-13 13:20:00 -0400

    1. add nprogress bar when page refresh

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,25 @@
-// import React from 'react'
+import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Layout = ({children}) => {
   const head = () => (
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossOrigin="anonymous"
-    />
-  )
+    <React.Fragment>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossOrigin="anonymous"
+      />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
+    </React.Fragment>
+  );
 
   const nav = () => (
     <ul className="nav nav-tabs bg-warning">
@@ -33,10 +42,10 @@ const Layout = ({children}) => {
   );
 
   return (
-    <>
+    <React.Fragment>
       {head()} {nav()}
       <div className="container pt-5 pb-5">{children}</div>
-    </>
+    </React.Fragment>
   );
 };
 
