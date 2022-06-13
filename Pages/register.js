3aca41879b608a6e6a1909fa01657c4983ce9414
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-13 15:03:09 -0400

    1. add Register page entry fields

--- Pages/register.js
+++ Pages/register.js
@@ -1,5 +1,33 @@
 import Layout from "../components/Layout";
 
-const Register = () => <Layout>register page</Layout>;
+const Register = () => {
+  const registerForm = () => (
+    <form>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Type your name" />
+      </div>
+      <div className="form-group">
+        <input type="email" className="form-control" placeholder="Type your email" />
+      </div>
+      <div className="form-group">
+        <input type="password" className="form-control" placeholder="Type your password" />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-outline-warning">Register</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <h1>Register</h1>
+          <br />
+          {registerForm()}
+          <hr />
+      </div>
+    </Layout>
+  );
+};
 
 export default Register;
