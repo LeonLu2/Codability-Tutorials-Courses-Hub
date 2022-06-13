65874b319c1eb111600af458e39bde6d38c3ba76
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-13 12:30:54 -0400

    1. edit header style and text message place

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,41 @@
 // import React from 'react'
+import Head from 'next/head';
 
-const Layout = ({ children }) => {
-    const nav = () => (
-        <ul className="nav">
-            <li className="nav-item">
-                <a className="nav-link" href="">
-                    Home
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="">
-                    Login
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="">
-                    Register
-                </a>
-            </li>
-        </ul>
-    );
+const Layout = ({children}) => {
+  const head = () => (
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossOrigin="anonymous"
+    />
+  )
 
-    return (
-        <>
-            {nav()} {children}
-        </>
-    );
+  const nav = () => (
+    <ul className="nav nav-tabs bg-warning">
+      <li className="nav-item">
+        <a className="nav-link text-dark" href="">
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-dark" href="">
+          Login
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-dark" href="">
+          Register
+        </a>
+      </li>
+    </ul>
+  );
+
+  return (
+    <>
+      {head()} {nav()} <div className="container pt-5 pb-5">{children}</div>
+    </>
+  );
 };
 
 export default Layout;
