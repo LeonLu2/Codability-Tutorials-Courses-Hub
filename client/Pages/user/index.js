294ead42e20afb39c66bc9ab892b856391aac0f4
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-16 18:01:00 -0400

    1. add methods to get cookie from browser and server 2. get user info using SSR for authorized user after login only, if no user, no info

--- client/Pages/user/index.js
+++ client/Pages/user/index.js
@@ -1,15 +1,25 @@
-import {useEffect, useState} from 'react';
 import Layout from '../../components/Layout';
 import axios from 'axios';
+import {API} from '../../config';
+import {getCookie} from '../../helpers/auth';
 
-const User = ({todos}) => <Layout>{JSON.stringify(todos)}</Layout>;
+const User = ({user}) => <Layout>{JSON.stringify(user)}</Layout>;
 
-User.getInitialProps = async () => {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
-  console.log('SERVER RENDERED', response);
-  return {
-    todos: response.data
-  };
+User.getInitialProps = async context => {
+  const token = getCookie('token', context.req);
+  try {
+    const response = await axios.get(`${API}/user`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        contentType: 'application/json'
+      }
+    });
+    return {user: response.data};
+  } catch (error) {
+    if (error.response.status === 401) {
+      return {user: 'no user'};
+    }
+  }
 };
 
 export default User;
