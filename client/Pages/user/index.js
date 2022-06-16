969114bd6599421a034bb261c37defd0181e6f6d
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-16 17:32:03 -0400

    1. use getInitialProps to realize Server Size Rendering, and it renders first time in server, after that it renders in browser console

--- client/Pages/user/index.js
+++ client/Pages/user/index.js
@@ -1,5 +1,15 @@
-import Layout from "../../components/Layout";
+import {useEffect, useState} from 'react';
+import Layout from '../../components/Layout';
+import axios from 'axios';
 
-const User = () => <Layout>hello user</Layout>;
+const User = ({todos}) => <Layout>{JSON.stringify(todos)}</Layout>;
+
+User.getInitialProps = async () => {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+  console.log('SERVER RENDERED', response);
+  return {
+    todos: response.data
+  };
+};
 
 export default User;
