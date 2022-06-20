5a73c2c8109c69bbe504cd5c801a223feaddf77c
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-20 15:09:35 -0400

    1. show all categories on the homepage

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,7 +1,7 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
 const formidable = require('formidable');
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
@@ -63,29 +63,36 @@ exports.create = (req, res) => {
 };
 
 // exports.create = (req, res) => {
-//     const { name, content } = req.body;
-//     const slug = slugify(name);
-//     const image = {
-//         url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
-//         key: '123'
-//     };
-
-//     const category = new Category({ name, slug, image });
-//     category.postedBy = req.user._id;
-
-//     category.save((err, data) => {
-//         if (err) {
-//             console.log('CATEGORY CREATE ERR', err);
-//             return res.status(400).json({
-//                 error: 'Category create failed'
-//             });
-//         }
-//         res.json(data);
-//     });
+//   const {name, content} = req.body;
+//   const slug = slugify(name);
+//   const image = {
+//     url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
+//     key: '123'
+//   };
+//
+//   const category = new Category({name, slug, image});
+//   category.postedBy = req.user._id;
+//
+//   category.save((err, data) => {
+//     if (err) {
+//       console.log('CATEGORY CREATE ERR', err);
+//       return res.status(400).json({
+//         error: 'Category create failed'
+//       });
+//     }
+//     res.json(data);
+//   });
 // };
 
 exports.list = (req, res) => {
-  //
+  Category.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Categories could not load'
+      });
+    }
+    res.json(data);
+  });
 };
 
 exports.read = (req, res) => {
