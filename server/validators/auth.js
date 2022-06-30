2f4beb44db11771322edabeafc2c17de6779fbf9
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-30 16:49:06 -0400

    1. add interested category checkbox in server, and save the categories with in the user info

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -10,7 +10,10 @@ exports.userRegisterValidator = [
     .withMessage('Must be a valid email address'),
   check('password')
     .isLength({min: 6})
-    .withMessage('Password must be at least 6 characters long')
+    .withMessage('Password must be at least 6 characters long'),
+  check('categories')
+    .isLength({min: 6})
+    .withMessage('Pick at least one category')
 ];
 
 exports.userLoginValidator = [
